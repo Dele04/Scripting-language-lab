64a8174185267928b1b5294e1222b11e3ad26390
Dele04 <trentderreck2@gmail.com>
2026-03-17 19:35:03 +0100

    complete lab3.1.6

---
+++ pe1-module3/lab_3_1_6_qns_ans.py
@@ -0,0 +1,3 @@
+#write a simple two-line program that takes the parameter n as input, which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+n = int(input("Enter a number: "))
+print(n>=100)
