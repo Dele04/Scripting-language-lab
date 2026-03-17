9c2f58fc5f219b1222841788f95ac58c2efce43f
Dele04 <trentderreck2@gmail.com>
2026-03-17 21:06:37 +0100

    complete lab3.1.11

---
+++ pe1-module3/lab_3_1_11_if_else.py
@@ -0,0 +1,13 @@
+#Your task is to write a tax calculator
+#It should accept one floating-point value: the income.
+#Next, it should print the calculated tax, rounded to full thalers. There's a function named round() which will do the rounding
+
+n = float(input("Enter an income: "))
+if n < 85528:
+    tax = 0.18 * n - 556.02
+else:
+    tax = 14839.02 + 0.32 * (n - 85528)
+if tax < 0:
+	tax = 0.0
+tax = round(tax, 0)
+print("The tax is:", tax , "thalers")
