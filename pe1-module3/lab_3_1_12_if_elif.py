c14cd78eea7ae7c259d4185722dad8e884f01595
Dele04 <trentderreck2@gmail.com>
2026-03-17 21:39:24 +0100

    complete lab3.1.12

---
+++ pe1-module3/lab_3_1_12_if_elif.py
@@ -0,0 +1,17 @@
+#determine kind of year
+#if the year number isn't divisible by four, it's a common year;
+#otherwise, if the year number isn't divisible by 100, it's a leap year;
+#otherwise, if the year number isn't divisible by 400, it's a common year;
+#otherwise, it's a leap year.
+year = int(input("Enter a year: "))
+
+if year < 1582:
+	print("Not within the Gregorian calendar period")
+elif year % 4 != 0:
+    print("Common year")
+elif year % 100 != 0:
+    print("Leap year")
+elif year % 400 != 0:
+    print("Common year")
+else:
+        print("Leap year")
