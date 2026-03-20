cbe644484817fc6c0af85253ca1d622cd39788b1
Dele04 <trentderreck2@gmail.com>
2026-03-20 22:50:01 +0100

    complete lab3.1.10

--- pe1-module3/lab_3_1_10_comparison_condition.py
+++ pe1-module3/lab_3_1_10_comparison_condition.py
@@ -1,4 +1,5 @@
-#Imagine that your computer program loves these plants. Whenever it receives an input in the form of the word Spathiphyllum, it involuntarily shouts to the console the following string: "Spathiphyllum is the best plant ever!"
+#Imagine that your computer program loves these plants. Whenever it receives an input in the form of the word Spathiphyllum,#
+# it involuntarily shouts to the console the following string: "Spathiphyllum is the best plant ever!"
 n = (input("Enter a name: "))
 if n == "Spathiphyllum":
     print("Yes - Spathiphyllum is the best")
