8853a93dad0048c8270ba86a7e011ed091b867a3
Dele04 <trentderreck2@gmail.com>
2026-05-27 23:29:16 +0200

    lab2.0.15

---
+++ je1-module2/lab_2_0_15_variables.js
@@ -0,0 +1,20 @@
+//First person
+let name1 = "Maxwell Wright";
+let phone1 = "(0191) 719 6495";
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+
+//Second person
+let name2 = "Raja Villarreal";
+let phone2 = "0866 398 2895";
+let email2 = "posuere.vulputate@sed.com";
+
+//Third person
+let name3 = "Helen Richards";
+let phone3 = "0800 1111";
+let email3 = "libero@convallis.edu";
+
+//Display first person
+console.log(name1 + "/" + phone1 + "/" + email1);
+
+//Display last contact
+console.log(name3 + "/" + phone3 + "/" + email3);
